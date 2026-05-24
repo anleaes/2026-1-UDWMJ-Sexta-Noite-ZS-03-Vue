7fac5f4d2f02cabba76e33804e1b66931872d374
augusto-dmh <augustodemelohenriques@gmail.com>
2026-05-24 20:19:00 -0300

    finaliza serializer do enderecos

--- apps/enderecos/serializer.py
+++ apps/enderecos/serializer.py
@@ -1,3 +1,7 @@
+from .models import Endereco
 from rest_framework import serializers
 
-# TODO: import model and create serializer
+class EnderecoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Endereco
+        fields = '__all__'
