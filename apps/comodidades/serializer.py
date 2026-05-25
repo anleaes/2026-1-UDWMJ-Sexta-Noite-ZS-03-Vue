bad324430005b5e146bfbdc30ad1f9fc531d815f
augusto-dmh <augustodemelohenriques@gmail.com>
2026-05-24 21:22:00 -0300

    finaliza serializer do comodidades

--- apps/comodidades/serializer.py
+++ apps/comodidades/serializer.py
@@ -1,3 +1,7 @@
+from .models import Comodidade
 from rest_framework import serializers
 
-# TODO: import model and create serializer
+class ComodidadeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comodidade
+        fields = '__all__'
