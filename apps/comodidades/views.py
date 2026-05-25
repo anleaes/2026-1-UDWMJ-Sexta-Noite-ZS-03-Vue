18a945217877490b4e5cf3a0a00d43a58b19d86f
augusto-dmh <augustodemelohenriques@gmail.com>
2026-05-24 22:43:00 -0300

    adiciona autenticacao token no comodidades

--- apps/comodidades/views.py
+++ apps/comodidades/views.py
@@ -1,7 +1,11 @@
 from rest_framework import viewsets
-from .models import Comodidades
-from .serializer import ComodidadesSerializer
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from .models import Comodidade
+from .serializer import ComodidadeSerializer
 
-class ComodidadesViewSet(viewsets.ModelViewSet):
-    queryset = Comodidades.objects.all()
-    serializer_class = ComodidadesSerializer
+class ComodidadeViewSet(viewsets.ModelViewSet):
+    queryset = Comodidade.objects.all()
+    serializer_class = ComodidadeSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
