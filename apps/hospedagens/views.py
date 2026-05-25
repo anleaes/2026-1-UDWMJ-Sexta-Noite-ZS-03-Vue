a1f69e3eb08e906f82393091e893bbcb0e5bd1da
augusto-dmh <augustodemelohenriques@gmail.com>
2026-05-25 20:59:00 -0300

    adiciona autenticacao token no hospedagens

--- apps/hospedagens/views.py
+++ apps/hospedagens/views.py
@@ -1,7 +1,11 @@
 from rest_framework import viewsets
-from .models import Hospedagens
-from .serializer import HospedagensSerializer
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from .models import Hospedagem
+from .serializer import HospedagemSerializer
 
-class HospedagensViewSet(viewsets.ModelViewSet):
-    queryset = Hospedagens.objects.all()
-    serializer_class = HospedagensSerializer
+class HospedagemViewSet(viewsets.ModelViewSet):
+    queryset = Hospedagem.objects.all()
+    serializer_class = HospedagemSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
